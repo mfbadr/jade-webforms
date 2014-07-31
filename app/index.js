5b9bf736f16ed0d6fd670b930a74b35cf0d9561c
Mikey Badr <mfbadr@gmail.com>
2014-07-31 15:10:32 -0500

    added boxes

--- app/index.js
+++ app/index.js
@@ -40,9 +40,43 @@ app.post('/calc', function (req, res){
       result = (req.body.x * 1) / (req.body.y * 1);
       break;
   }
-  
   res.render('calc', {result:result, x:req.body.x, y:req.body.y, equals:'=', op:req.body.op});
 });
+
+app.get('/boxes', function (req, res){
+  res.render('box1');
+});
+
+app.post('/boxes', function (req, res){
+  //turn colors into array of strings
+  var colors = req.body.colors.split(',');
+  // turn boxes to a number 
+  var boxes = parseInt(req.body.boxes);
+  //turn string of width range into two numbers
+  var widthRange = req.body.width.split('-');
+  var widthMin = parseInt(widthRange[0]);
+  var widthMax = parseInt(widthRange[1]);
+  //turn string of height range into two numbers
+  var heightRange = req.body.height.split('-');
+  var heightMin = parseInt(heightRange[0]);
+  var heightMax = parseInt(heightRange[1]);
+  console.log(colors, '# boxes =' + boxes, 'width is from', widthMin + '-' + widthMax, 'height is from', heightMin, '-', heightMax);
+    
+  var allBoxes = [];
+  
+
+  for( var i = 0; i < boxes; i++){
+    var thisBox = {};
+    thisBox.width = Math.floor(Math.random() * ( (  widthMax - widthMin) +1  ) + widthMin )  ;
+    thisBox.height = Math.floor(Math.random() *( ( heightMax - heightMin) +1 ) + heightMin );
+    var colorNum = Math.floor(Math.random() * colors.length);
+    thisBox.color = colors[colorNum];
+    allBoxes.push(thisBox);
+  }
+//  console.log(allBoxes); 
+  res.render('box2', {boxes:allBoxes});
+});
+  
 
 var port = process.env.PORT;
 
