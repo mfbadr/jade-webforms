3d2efa47892bc6106cff927042416444e5e7795f
Mikey Badr <mfbadr@gmail.com>
2014-07-31 12:05:24 -0500

    added calculator

--- app/index.js
+++ app/index.js
@@ -2,15 +2,50 @@
 
 var express = require('express');
 var morgan = require('morgan');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
+
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/static'));
 
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.get('/', function (req, res){
   res.render('home');
+});
+
+app.get('/calc', function (req, res){
+  res.render('calc');
+});
+
+
+app.post('/calc', function (req, res){
+  var result;
+  switch(req.body.op){
+    case '+':
+      result = (req.body.x * 1) + (req.body.y * 1);
+      break;
+    case '-':
+      result = (req.body.x * 1) - (req.body.y * 1);
+      break;
+    case '*':
+      result = (req.body.x * 1) * (req.body.y * 1);
+      break;
+    case '/':
+      result = (req.body.x * 1) / (req.body.y * 1);
+      break;
+  }
+  
+  res.render('calc', {result:result, x:req.body.x, y:req.body.y, equals:'=', op:req.body.op});
+});
+
+var port = process.env.PORT;
+
+app.listen(port, function(){
+  console.log('Express is listening on PORT', port + '...');
 });
